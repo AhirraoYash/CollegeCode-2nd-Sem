f5834809a09bb2a6caa490c3db04ad7408483621
Yash Ahirrao <yashahirrao204@gmail.com>
2025-02-07 10:59:02 +0530

    commitd

---
+++ Python/Theory/all_Basic.py
@@ -0,0 +1,74 @@
+# x="A"
+# if x=="B":
+#     print("B")
+# else:
+#     if(x=="c"):
+#         print("C")
+#     else:
+#         if(x=="A"):
+#             print("A")
+#         else:
+#             print("Letter is not A B C")
+
+
+
+# num=10
+# if num>5:
+#     print("number is greter than 5")
+#     if(num<=15):
+#         print("Betwee 5 and 15")
+
+
+
+# elif program
+# letter="A"
+# if letter=="B":
+#     print("letter is B")
+# elif(letter=="C"):
+#     print("Letter is C")
+# elif(letter=="A"):
+#     print("letter is A")
+
+
+# number=50
+# if((number>=50) & (number<=75)):
+#     print("Pass")
+# if(number>=75):
+#     print("1st number")
+
+# Operator in python
+# 1] Addtion Operator : in python + is the Addtion operator
+# 2] Subtraction : -
+# 3] modules operator : % give reminder
+# 4] Expontiontion oerator : ** power of operator
+#      example
+# print(2**2)
+
+# 5] floor Division // : user to find the queation whenn first oop4
+# print(3//2)
+
+# 6] logical operator : and , or ,
+# a=10
+# c=-10
+# b=10
+# if a>=c and a>=b:
+#     print("A is greter")
+# if a>0 and b>0:
+#     print("a and b is greter than 0")
+# else:
+#     print("No number is greter than 0")
+
+# 7] Not operator : not
+# if( not a):
+#     print(a)
+# else:
+#     print("a hello")
+
+# 8] is operator use to comparizen
+# if 2 is 2:
+#     print("hello")
+
+num=1
+while num<=5 :
+    print(num)
+    num+=1
